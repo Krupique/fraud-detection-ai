51dcd515b0f2d67369055059f6eac5e21ed6f708
Krupique <krupique@outlook.com>
2025-02-14 08:14:54 -0300

    feat: get data from database

---
+++ app/04-RunLLM.py
@@ -0,0 +1,78 @@
+# Python - Insights Extraction Pipeline with LLM
+
+# Imports
+import csv
+import psycopg2
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.llms.ollama import Ollama
+
+# Instantiating LLM Llama3 through Ollama
+llm = Ollama(model = "llama3")
+
+# Creating the parser for the language model output
+output_parser = StrOutputParser()
+
+# Function to generate text based on PostgreSQL data
+def generate_insights():
+
+    # Connect to the PostgreSQL database with the provided credentials
+    conn = psycopg2.connect(
+        dbname="transactiondb",
+        user="admin",
+        password="admin1010",
+        host="localhost",
+        port="5553"
+    )
+
+    # Create a cursor to execute SQL commands
+    cursor = conn.cursor()
+
+    # Define the SQL query to retrieve customer, property and transaction data
+    query = """
+        SELECT t.transaction_id     AS transaction_id, 
+               c.name               AS customer_name, 
+               i.description        AS property_description,
+               t.value              AS value_property, 
+               t.transaction_date   AS transaction_date, 
+               t.transaction_type   AS transaction_type, 
+               t.status             AS status, 
+               h.modification_date  AS modification_date, 
+               h.description        AS history_description
+        FROM projectdb.financial_transactions t
+        JOIN projectdb.customers c 
+          ON t.customer_id = c.customer_id
+        JOIN projectdb.properties i 
+          ON t.property_id  = i.property_id
+        JOIN projectdb.transactions_history h 
+          ON t.transaction_id = h.transaction_id
+        
+        WHERE t.value > 1000000  -- high value may be indicative of fraud
+           OR (t.status = 'Completed' AND h.description LIKE '%Canceled%')  -- cancellation after completion may be suspicious
+           OR EXISTS (  -- checks if there are multiple transactions for the same property in a short period of time
+                        SELECT 1 FROM projectdb.financial_transactions t2
+                        WHERE t2.property_id = t.property_id
+                          AND t2.transaction_id != t.transaction_id
+                          AND ABS(EXTRACT(DAY FROM AGE(t2.transaction_date, t.transaction_date))) < 30)
+        ORDER BY t.transaction_date DESC;
+    """
+
+    # Execute the SQL query
+    cursor.execute(query)
+
+    # Get all the results of the query
+    rows = cursor.fetchall()
+
+
+
+
+
+
+
+
+
+
+
+
+# Gera insights chamando a função definida
+insights = generate_insights()
